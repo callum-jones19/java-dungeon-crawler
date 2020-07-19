eae1e7bc64d00ec239c9e70b61ea3b25808f8deb
Nikhil Ahuja <nikhilahuja314@gmail.com>
2020-07-19 23:20:40 +1000

    Updated tests

--- src/test/PotionTest.java
+++ src/test/PotionTest.java
@@ -1,5 +1,16 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.Enemy;
+import unsw.dungeon.Wall;
+import unsw.dungeon.Player;
+import unsw.dungeon.Boulder;
+import unsw.dungeon.Sword; 
+
 public class PotionTest {
     
 }
