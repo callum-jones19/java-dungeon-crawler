95aa432c0c43f550c8d6a40e47433688f41b4e61
Callum Jones <emailcallumj@gmail.com>
2020-07-20 18:12:07 +1000

    Added first basic test for Potion

--- src/test/PotionTest.java
+++ src/test/PotionTest.java
@@ -5,12 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import unsw.dungeon.Dungeon;
-import unsw.dungeon.Enemy;
-import unsw.dungeon.Wall;
+import unsw.dungeon.DungeonState;
 import unsw.dungeon.Player;
-import unsw.dungeon.Boulder;
-import unsw.dungeon.Sword; 
+import unsw.dungeon.Potion;
 
 public class PotionTest {
     
+    @Test
+    public void test() {
+        Dungeon d = new Dungeon(5, 5);
+        Potion pot = new Potion(2, 2);
+        d.addEntity(pot);
+        Player p = new Player(d, 1, 2);
+        d.setPlayer(p);
+        p.moveRight();
+
+        assertEquals(true, p.isInvincible());
+
+        DungeonState ds = new DungeonState(d);
+        ds.run(11);
+
+        assertEquals(false, p.isInvincible());
+    }
 }
